bf99fb61d5a7957f4b1d81d3ec9e58a10b9c600c
Jason Li <Yunf.li37@gmail.com>
2017-12-05 20:39:18 -0500

    self balancing tree

--- Trees/my_binary_tree.py
+++ Trees/my_binary_tree.py
@@ -1,5 +1,7 @@
 from binarytree import Node, show
 import numpy.random as nprnd
+from collections import deque
+
 class myTreeNode(Node):
 	def __init__(self, value):
 		self.value = value
@@ -24,12 +26,12 @@ class myTreeNode(Node):
 			return True
 		else:
 			if self.value > value_toSearchFor:
-				if(self.left is None):
+				if self.left is None:
 					return False
 				else:
 					return self.left.exists(value_toSearchFor)
 			elif self.value < value_toSearchFor:
-				if(self.right is None):
+				if self.right is None:
 					return False
 				else:
 					return self.right.exists(value_toSearchFor)
@@ -48,8 +50,39 @@ class myTreeNode(Node):
 			return left_subtree_height +1
 		else:
 			return right_subtree_height +1
-			
+	
+	def is_root(self):
+		return self.parent is None
 
+	def is_right_child(self):
+		if self.is_root(): return False
+		return self.value >= self.parent.value
+	def is_left_child(self):
+		if self.is_root(): return False
+		return self.value < self.parent.value
+
+
+
+	def left_rotate(self):
+		print "Starting Left Rotation"
+
+		#check to see if you can actually do the rotation
+		# check for null parent
+		if self.is_root() or self.is_right_child():
+			return
+		#assign all the variable you'll be using
+		pivot = self
+		parent_of_pivot = self.parent
+		left_of_pivot = self.left
+		# can only rotate a right child
+		if self.is_left_child():
+			parent_of_pivot =pivot
+			pivot.left = parent_of_pivot
+			parent_of_pivot.right = left_of_pivot
+			left_of_pivot.parent = parent.right
+			# The paren'ts left child because the right child of the pivot
+			# Pivot becomes the parent's parent
+		print "Done with Left Roatation"
 # three ways to process the tree
 # in order : left self right
 # post order: left right self
@@ -85,13 +118,34 @@ class BST:
 		if self.right is not None:
 			self.print_in_order()
 
+
+	def test_rotate(self):
+		if self.root is None or self.root.right is None:
+			return
+		
+		self.root.right.left_rotate()
+
+	def print_level_order(self):
+		if self.root is None:
+			return
+		queue.deque()
+		queue.append(self.root)
+		while len(queue) !=0:
+			current_node = queue.popleft()
+			print current_node.value
+			if current_node.left is not None:
+				queue.append(current_node.left)
+			if current_node.right is not None:
+				queue.append(current_node.right)
 def random_numbers(total_numbers):
     return [int(1000 * nprnd.random()) for i in range(total_numbers)]
 
 tree = BST()
 for i in random_numbers(10):
-	tree.add(i)
+    tree.add(i)
 
-print tree.exists(199);
+# print tree.exists(199);
 show(tree.root)
-print tree.height();
+# print tree.height();
+tree.test_rotate()
+show(tree.root);
